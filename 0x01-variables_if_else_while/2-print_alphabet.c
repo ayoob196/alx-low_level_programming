4a50d0d098355046feb1580ed5598b7c1061372f
ayoob <ayoobsaad5@gmail.com>
2022-06-04 03:13:46 -0700

    file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - main does something
+ * Return: return ssomething
+ */
+
+int main(void)
+{
+
+	for ( char ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
