8e916766efcfb19653ec59b1c8f37aed0eb27db6
ayoob <ayoobsaad5@gmail.com>
2022-06-04 03:35:23 -0700

    file 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - main does something
+ *  Return: returns someting
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch == 'e' || ch == 'q') continue;
+		putchar(ch);
+	}
+	return(0);
+}
