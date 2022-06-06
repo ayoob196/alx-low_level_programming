7b8f0d033b62780c8a8326f3237a26bcbc3f71f4
ayoob <ayoobsaad5@gmail.com>
2022-06-06 03:34:44 -0700

    file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+* main - main
+* Return: hello
+*/
+int main(void)
+{
+	int start;
+
+	for (start = 48; start < 58; start++)
+	{
+		putchar(start);
+	}
+	putchar('\n');
+	return (0);
+}
