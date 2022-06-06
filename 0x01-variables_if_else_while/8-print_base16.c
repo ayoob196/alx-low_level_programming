a422d0f7d16f4b89717631c2a721a3315987e9a9
ayoob <ayoobsaad5@gmail.com>
2022-06-06 03:47:20 -0700

    file *

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - i'm tired
+ * Return: but i\m strong
+ */
+int main(void)
+{
+	int start;
+
+	for (start = 48; start < 58; start++)
+	{
+		putchar(start);
+	}
+	for (start = 97; start < 103; start++)
+	{
+		putchar(start);
+	}
+	putchar('\n');
+	return (0);
+}
