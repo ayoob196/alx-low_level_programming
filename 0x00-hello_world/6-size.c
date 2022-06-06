f7d9851aafa38b1b3efbb9ae9180c05206789c72
ayoob <ayoobsaad5@gmail.com>
2022-06-06 04:08:09 -0700

    file 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,8 @@
 #include<stdio.h>
-
-
 /**
  * main - size of some data types and format the output
- * Return: 0 if exited properly, non-zero otherwise 
+ * Return: 0 if exited properly, non-zero otherwise
  */
-
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
