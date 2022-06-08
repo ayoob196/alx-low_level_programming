cfa42eb8d02eefcbdb7f2de3572d56e799e2ba9d
ayoob <ayoobsaad5@gmail.com>
2022-06-07 17:41:53 -0700

    file 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#inlude"main.h"
+/**
+ * print_sign - he
+ * @n: hello
+ * Return: helo
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n < 0)
+	{
+		_putchar('-');
+		return(-1);
+	} else 
+	{
+		_putchar('0');
+		return (0);
+	}
+}
