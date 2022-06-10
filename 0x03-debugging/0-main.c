fdd489a98c58a715627f273d385d3cf1b8371b00
ayoob <ayoobsaad5@gmail.com>
2022-06-10 02:23:14 -0700

    file 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -8,19 +8,6 @@
  * @i: helo
  * return: helo
  */
-void positive_or_negative(int i)
-{
-	int n = i;
-
-	srand(time(0));
-	n = 0;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-}
 int main(void)
 {
 	int i = 0;
