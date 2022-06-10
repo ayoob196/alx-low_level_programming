ab0b4f9ed9ad99659d7a58366c52c76f98bb7a6b
ayoob <ayoobsaad5@gmail.com>
2022-06-10 10:24:47 -0700

    A

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 != 0)
@@ -27,8 +27,7 @@ int main(void)
 		{
 			printf(" %d", i);
 		}
-															}
+	}
 	printf("\n");
-	
 	return (0);
 }
