11e3e35f1b5e0cfa55c6ff3bc3c1c66474f6b7a4
ayoob <ayoobsaad5@gmail.com>
2022-06-09 23:40:43 -0700

    file 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,26 @@
-include "main.h"
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
  * main - tests function that prints if integer is positive or negative
- * Return: 0
+ * postitive_or_negative - helo
+ * @i: helo
+ * return: helo
  */
+void postitive_or_negative(int i)
+{
+	int n = i;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+}
 int main(void)
 {
 	int i = 0;
