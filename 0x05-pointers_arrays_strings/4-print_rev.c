3c6afc52e510c9410e0fafe94ba43405cbd93751
ayoob <ayoobsaad5@gmail.com>
2022-06-12 23:28:14 -0700

    a

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - sf
+ * @s: f
+ */
+
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+	while (s[len] != '\0')
+	{
+		i++;
+	}
+	len = i;
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
