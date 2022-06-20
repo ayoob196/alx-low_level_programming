ad241ee44a7b6b72265ced6bf87e4ee0667df089
ayoob <ayoobsaad5@gmail.com>
2022-06-20 02:35:31 -0700

    a

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * *_strchr - d
+ * @s: d
+ * @c: s
+ * Return: d
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+	}
+}
