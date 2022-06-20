afd12f6cabf012e8b05793954c96d48387e4caca
ayoob <ayoobsaad5@gmail.com>
2022-06-20 01:55:34 -0700

    a

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * *_memcpy - a
+ * @dest: d
+ * @src: d
+ * @n: d
+ * Return: w
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int count;
+
+	for (count = 0; count < n; count++)
+	{
+		*(dest + count) = *(src + count);
+	}
+}
