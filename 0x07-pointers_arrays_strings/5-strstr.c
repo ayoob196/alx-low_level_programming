da4f930d47a834021638791004957be2814c8c9d
ayoob <ayoobsaad5@gmail.com>
2022-06-20 11:14:03 -0700

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -14,8 +14,10 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i + j] != needle[j]);
-			break;
+			if (haystack[i + j] != needle[j])
+			{
+				break;
+			}
 		}
 		if (!needle[j])
 			return (&haystack[i]);
