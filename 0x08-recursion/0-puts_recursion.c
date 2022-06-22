3228328bc83f7740c0ce0da30900f3ef37e1edef
ayoob <ayoobsaad5@gmail.com>
2022-06-22 01:49:17 -0700

    a

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,15 @@
 #include "main.h"
 /**
- * _puts_recursion -a 
+ * _puts_recursion -a
  * @s: d
  */
 void _puts_recursion(char *s)
 {
-	if (*s)
+	if (*s == '\0')
 	{
-		_ptuchar(*s);
-		_puts_recursion(s + 1);
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
-	return;
+	_ptuchar(*s);
+	_puts_recursion(s + 1);
 }
