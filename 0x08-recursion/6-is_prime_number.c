8f0ce45f293a197ceadaf8b7732f82d4bd8cc3b2
ayoob <ayoobsaad5@gmail.com>
2022-06-22 06:05:00 -0700

    a

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * is_prime_number - a
+ * @n: d
+ * Return: f
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
+
+/**
+ * actual_prime - a
+ * @n: d
+ * @i: d
+ * Return: d
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
