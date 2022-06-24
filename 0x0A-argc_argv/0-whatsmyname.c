52f9b6102b8e8371ed9dc229f42e8e2b4dd8d14f
ayoob <ayoobsaad5@gmail.com>
2022-06-24 08:06:02 -0700

    a

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/** 
+ * main - a
+ * @argc: d
+ * @argv: d
+ * Return: d
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	
+	return (0);
+}
