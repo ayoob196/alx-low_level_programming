b70d11b2edfc42d07423e99e9e50176c32c49dfc
ayoob <ayoobsaad5@gmail.com>
2022-07-06 03:15:12 -0700

    a

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - c
+ * @a: r
+ * @b: r
+ * Return: b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - s
+ * @a: f
+ * @b: r
+ * Return: b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - s
+ * @a: f
+ * @b: r
+ * Return: s
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a
+ * @a: s
+ * @b: a
+ * Return: b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - a
+ * @a: a
+ * @b: s
+ * Return: a
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
