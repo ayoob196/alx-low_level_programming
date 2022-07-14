aa961105aa38446178452eed23c4ee39ea1f7d56
ayoob <ayoobsaad5@gmail.com>
2022-07-14 10:40:41 -0700

    first commit all files

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * list_len - a
+ * @h: a
+ * Return: a
+ */
+size_t list_len(const list_t *h)
+{
+	size_t n = 0;
+	
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+	return (n);
+}
