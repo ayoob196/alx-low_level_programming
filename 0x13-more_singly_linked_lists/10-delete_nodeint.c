8f92c3a64951a8c4a21f711b2ec49e66622272ee
ayoob <ayoobsaad5@gmail.com>
2022-07-19 10:31:04 -0700

    fix a typo

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp = *head;
 	listint_t *current = NULL;
 	unsigned int i = 0;
-	
+
 	if (*head == NULL)
 		return (-1);
 	if (index == 0)
@@ -29,6 +29,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	current = temp->next;
 	temp->next = current->next;
-	fee(current);
+	free(current);
 	return (1);
 }
