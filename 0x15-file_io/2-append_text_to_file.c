1ca48654a0e68ad9a4491cdf77b2d4fdc2957fd6
ayoob <ayoobsaad5@gmail.com>
2022-07-28 03:17:39 -0700

    first commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - a
+ * @filename: a
+ * @text_content: a
+ * Return: a
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, t, s = 0;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[s])
+			s++;
+		t = write(fd, text_content, s);
+		if (t != s)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
