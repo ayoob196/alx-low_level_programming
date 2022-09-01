fac3e931d7283ae46583dc135c39298a7c2aaded
ayoob <ayoobsaad5@gmail.com>
2022-09-01 13:23:43 -0700

    add all files

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -8,6 +8,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t nodes = 0;
+
 	if (!h)
 		return (0);
 	while (h)
--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,6 +10,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 	dlistint_t *temp = *head;
+
 	if (!head)
 		return (NULL);
 	new = malloc(sizeof(dlistint_t));
